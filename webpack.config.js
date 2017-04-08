88cd6d3745aed02391b07dbc6a9c108ee5f47354
szepg <gergely.szep@betvictor.com>
2017-04-08 15:10:20 +0200

    Remove react

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    entry: "./src/index.tsx",
+    entry: "./src/index.ts",
     output: {
         filename: "bundle.js",
         path: __dirname + "/dist"
@@ -28,7 +28,6 @@ module.exports = {
     // This is important because it allows us to avoid bundling all of our
     // dependencies, which allows browsers to cache those libraries between builds.
     externals: {
-        "react": "React",
-        "react-dom": "ReactDOM"
+        "phaser-ce": "Phaser"
     },
 };
